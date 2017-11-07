cb7508b9990c19c453dbca5cacc8bc5201a1790a
USER <104021097@live.asia.edu.tw>
2017-11-07 12:24:39 +0800

    ３

--- src/test_1.java
+++ src/test_1.java
@@ -1,21 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-
+//姓名  10分
 public class test_1 extends JFrame {
     private JLabel jlname = new JLabel("資工系劉佳媛");
-    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-    private int frmW = 300, frmH = 150, screenW, screenH;
+
+
     private Container cp;
 
     public test_1(){
         initComp();
     }
     private void initComp(){
-        screenW = dim.width;
-        screenH = dim.height;
-        this.setBounds(screenW/2-frmW/2,screenH/2-frmH/2,frmW,frmH);
+
+        this.setBounds(100,100,300,300);
         cp =this.getContentPane();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jlname.setBounds(100,100,80,30);
+this.setLayout(null);
         jlname.setHorizontalAlignment(JLabel.CENTER);
         jlname.setBackground(Color.CYAN);
         jlname.setOpaque(true);
